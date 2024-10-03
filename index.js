1ad72ab699c54f7ce266edbf791f14850ae14353
ekramulislam2050 <ekramulislam2050@gmail.com>
2024-10-03 15:33:14 +0600

    added video thumbnail

--- index.js
+++ index.js
@@ -1,23 +1,63 @@
 //  fetch data categories and display
-const loadCategories = async()=>{
-   try{
-    const res = await fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
-    const data = await res.json()
-    const categoriesOfData =  data.categories
-     displayData(categoriesOfData)
-   }
-   catch(error){ 
+const loadCategories = async () => {
+    try {
+        const res = await fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
+        const data = await res.json()
+        const categoriesOfData = data.categories
+        displayData(categoriesOfData)
+    }
+    catch (error) {
         console.log(error)
-   }
+    }
 }
-const displayData=(categoriesOfData)=>{
-     categoriesOfData.forEach(item=>{
-       const btnContainer = document.getElementById("categories");
-       const button = document.createElement("button");
-       button.classList.add("btn");
-       button.innerText = item.category;
-       btnContainer.appendChild(button)
-     })
+const displayData = (categoriesOfData) => {
+    categoriesOfData.forEach(item => {
+        const btnContainer = document.getElementById("categories");
+        const button = document.createElement("button");
+        button.classList.add("btn");
+        button.innerText = item.category;
+        btnContainer.appendChild(button)
+    })
+
+}
+loadCategories();
+// data fetch for videos
+const fetchVideos = async () => {
+  try{
+    const dataOfVideos = await fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+    const data = await dataOfVideos.json()
+    const videos = data.videos
+    displayVideos(videos)
+  }
+  catch(error){
+        console.log(error)
+  }
+
+}
+// display videos
+const displayVideos = (videos) => {
+    videos.forEach((video) => {
+        const cardContainer = document.getElementById("card-container");
+        const div = document.createElement("div")
+        div.classList.add = ("card card-compact")
+        div.innerHTML =
+            `
+                <figure>
+                    <img
+                    src=${video.thumbnail} 
+                    alt="Shoes" />
+                </figure>
+                <div class="card-body">
+                    <h2 class="card-title">Shoes!</h2>
+                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <div class="card-actions justify-end">
+                    <button class="btn btn-primary">Buy Now</button>
+                    </div>
+                </div>
     
+       
+       `
+       cardContainer.appendChild(div)
+    })
 }
-loadCategories()
+fetchVideos()
