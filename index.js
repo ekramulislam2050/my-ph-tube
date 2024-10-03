eee812e25be95e5d4fe086a105bfeb32e8d2c12c
ekramulislam2050 <ekramulislam2050@gmail.com>
2024-10-03 11:35:30 +0600

    data fetch,load and create button dainamically

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
- 
+//  fetch data categories and display
+const loadCategories = async()=>{
+   try{
+    const res = await fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
+    const data = await res.json()
+    const categoriesOfData =  data.categories
+     displayData(categoriesOfData)
+   }
+   catch(error){ 
+        console.log(error)
+   }
+}
+const displayData=(categoriesOfData)=>{
+     categoriesOfData.forEach(item=>{
+       const btnContainer = document.getElementById("categories");
+       const button = document.createElement("button");
+       button.classList.add("btn");
+       button.innerText = item.category;
+       btnContainer.appendChild(button)
+     })
+    
+}
+loadCategories()
