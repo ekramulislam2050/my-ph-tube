0d4861ed048c0a5add350fcc63373e241b953f0b
ekramulislam2050 <ekramulislam2050@gmail.com>
2024-10-05 18:43:20 +0600

    added functionality to input field

--- index.js
+++ index.js
@@ -13,105 +13,105 @@ const loadCategories = async () => {
 const displayData = (categoriesOfData) => {
     const btnContainer = document.getElementById("categories");
     categoriesOfData.forEach(item => {
-       
+
         const div = document.createElement("div");
-        div.innerHTML=`
+        div.innerHTML = `
             <button class="btn categories-btn" id="${item.category_id}" onclick="categoriesOfVideo( ${item.category_id})">
                 ${item.category}
             </button>
         `
-        
+
         btnContainer.appendChild(div)
     })
 
 }
 loadCategories();
 // data fetch for videos
-const fetchVideos = async () => {
-  try{
-    const dataOfVideos = await fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
-    const data = await dataOfVideos.json()
-    const videos = data.videos
-     displayVideos(videos)
-  }
-  catch(error){
+const fetchVideos = async (searchTxt="") => {
+    try {
+        const dataOfVideos = await fetch(`https://openapi.programming-hero.com/api/phero-tube/videos?title=${searchTxt}`)
+        const data = await dataOfVideos.json()
+        const videos = data.videos
+        displayVideos(videos)
+    }
+    catch (error) {
         console.log(error)
-  }
+    }
 
 }
-const addRemove = ()=>{
+const addRemove = () => {
     const buttons = document.getElementsByClassName("categories-btn");
-    for(let btn of buttons){
+    for (let btn of buttons) {
         btn.classList.remove("active")
     }
 }
 // load categories of videos
-const categoriesOfVideo = async(id)=>{
+const categoriesOfVideo = async (id) => {
 
-    try{
+    try {
         const res = await fetch(`https://openapi.programming-hero.com/api/phero-tube/category/${id}`)
         const data = await res.json()
         const video = data.category
-        addRemove ()
-         const activeBtn = document.getElementById(id);
-         activeBtn.classList.add("active")
-         displayVideos(video)
-      }
-      catch(error){
-            console.log(error)
-      }
-    
+        addRemove()
+        const activeBtn = document.getElementById(id);
+        activeBtn.classList.add("active")
+        displayVideos(video)
+    }
+    catch (error) {
+        console.log(error)
+    }
+
 }
 
 // convert time
-const timeConverter = (time) =>{
-     const hours = parseInt(time / 3600);
-     const  remainingSecond = time % 3600;
-     const minute = parseInt( remainingSecond / 60);
-     const second = remainingSecond % 60;
-     return `${hours} hour ${minute} minute ${second} second ago`
+const timeConverter = (time) => {
+    const hours = parseInt(time / 3600);
+    const remainingSecond = time % 3600;
+    const minute = parseInt(remainingSecond / 60);
+    const second = remainingSecond % 60;
+    return `${hours} hour ${minute} minute ${second} second ago`
 }
 // load details
-const loadDetails=async(videoId)=>{
-     const uri = `https://openapi.programming-hero.com/api/phero-tube/video/${videoId}`
-     try{
+const loadDetails = async (videoId) => {
+    const uri = `https://openapi.programming-hero.com/api/phero-tube/video/${videoId}`
+    try {
         const res = await fetch(uri)
         const details = await res.json()
         displayDetails(details)
-     }
-     catch(error){
+    }
+    catch (error) {
         console.log(error)
-     }
-    
+    }
+
 }
 // display details
-const displayDetails=(details)=>{
+const displayDetails = (details) => {
 
-    
-     document.getElementById("videoDetails").innerHTML=`
+
+    document.getElementById("videoDetails").innerHTML = `
         <img src=${details.video.thumbnail}/>
         <p> ${details.video.description}</p>
      `
-     document.getElementById("modalForDetails").showModal();
+    document.getElementById("modalForDetails").showModal();
 }
 // display videos
 const displayVideos = (videos) => {
     const cardContainer = document.getElementById("card-container");
-     cardContainer.innerHTML='';
-     if(videos.length == 0){
+    cardContainer.innerHTML = '';
+    if (videos.length == 0) {
         cardContainer.classList.remove("grid")
-        cardContainer.innerHTML=` 
+        cardContainer.innerHTML = ` 
                 <div class='min-h-[400px] flex flex-col w-full justify-center items-center'>
                   <img src="./asset/Icon.png"/>
                   <h2 class="text-xl font-bold text-center">No content here in this category </h2>
                      
                </div>
                `
-              
-      
-     }else{
+
+
+    } else {
         cardContainer.classList.add("grid")
-     }
+    }
     videos.forEach((video) => {
         const div = document.createElement("div")
         div.classList.add = ("card card-compact")
@@ -121,23 +121,23 @@ const displayVideos = (videos) => {
                     <img class="h-full w-full object-cover"
                     src=${video.thumbnail} 
                     alt="Shoes" />
-                    ${video.others.posted_date.length == 0?'':`<span class="absolute right-4 bottom-2 bg-black text-white rounded p-1 text-xs"> ${timeConverter(video.others.posted_date)}</span> `}
+                    ${video.others.posted_date.length == 0 ? '' : `<span class="absolute right-4 bottom-2 bg-black text-white rounded p-1 text-xs"> ${timeConverter(video.others.posted_date)}</span> `}
 
                     
                 </figure>
                 <div class="px-0 py-2 flex">
                   <div class = 'w-10 h-10'>
                       <img class="w-full h-full   rounded-full object-cover" src=${video.authors[0].
-                        profile_picture}/>
+                profile_picture}/>
                   </div>
                   <div class="">
                       <h2 class="font-bold pl-2">${video.title
-                        }</h2>
+            }</h2>
                         <div class="flex pl-2">
                            <p>${video.authors[0].profile_name} </p>
                            ${video.authors[0].
-                            verified == true?`<img class="w-5 h-5 pl-1" src="https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png"/>`:""
-                            }
+                verified == true ? `<img class="w-5 h-5 pl-1" src="https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png"/>` : ""
+            }
                            
                         </div>
                           <p> <button class="btn btn-sm btn-error" onclick="loadDetails('${video.video_id}')">details</button  </p>
@@ -149,8 +149,11 @@ const displayVideos = (videos) => {
     
        
        `
-       cardContainer.appendChild(div)
-       
+        cardContainer.appendChild(div)
+
     })
 }
+document.getElementById("search-input").addEventListener("keyup",(e)=>{
+    fetchVideos(e.target.value)
+})
 fetchVideos()
